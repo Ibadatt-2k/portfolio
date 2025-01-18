0bd66a6c8633e5fa32a4ba72cbb7e1eda26c9c35
Ibadatt-2k <isaulakh2004@gmail.com>
2025-01-17 19:14:55 -0800

    finally changing it to the right image
    fixed the error with a smaple image now using the correct one

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function addStar(){
 }
 Array (200).fill().forEach(addStar)
 
-const euphoraTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/Ibadatt-2k/portfolio/main/mars.jpg');
+const euphoraTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/Ibadatt-2k/portfolio/main/euphora.jpg');
 const euphora = new THREE.Mesh(
     new THREE.SphereGeometry(6, 66, 66),
     new THREE.MeshStandardMaterial({
