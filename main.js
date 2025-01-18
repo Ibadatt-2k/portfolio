f950d35a748b2a27a1c8084f239f1b6860b8892f
Ibadatt-2k <isaulakh2004@gmail.com>
2025-01-17 18:50:02 -0800

    Changes in the path
    changed ./euphora.jpg path to "./src/assets/euphora.jpg"

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function addStar(){
 }
 Array (200).fill().forEach(addStar)
 
-const euphoraTexture = new THREE.TextureLoader().load('./euphora.jpg');
+const euphoraTexture = new THREE.TextureLoader().load('./src/assets/euphora.jpg');
 const euphora = new THREE.Mesh(
     new THREE.SphereGeometry(6, 66, 66),
     new THREE.MeshStandardMaterial({
