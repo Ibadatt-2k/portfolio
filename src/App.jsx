93149ec822a242664e94b04b464725d0257856b6
Ibadatt-2k <isaulakh2004@gmail.com>
2025-12-15 21:51:29 -0800

    Aligning everything incentre

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 import Experience from './sections/Experience/Experience';
 function App() {
-  console.log('test')
-  return(
-  <>
-    <Hero />
-    <Projects />
-    <Experience />
-    <Skills />
-    <Contact />
-    <Footer />
-  </>
+  return (
+    <div className="app">
+      <Hero />
+      <Projects />
+      <Experience />
+      <Skills />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
